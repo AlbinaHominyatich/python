32ca60a6d843658e0dcd145f1b36587c1c879a0d
AlbinaHominyatich <hominyatichalbina@gmail.com>
2022-10-25 16:04:34 +0300

    FIRST COMMIT

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print('Hello world!')
+#lesson2
+class Student:
+    amound = 0
+    print("Є. Я тут")
+    def __init__(self, name = None):
+        self.name = name
+        Student.amound += 1
+    def __str__(self):
+        return f'мій логін {self.name}'
+
+class Teacher(Student):
+    print('А ну роби домашку!')
+
+dima = Student()
+andry = Student()
+albina = Teacher()
+dima = Student(name = 'DIMAGLOBUS')
+print(dima)
